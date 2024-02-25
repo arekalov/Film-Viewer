9fa2b9c43ff9c68eb5e652bd0a61ab9bfc087486
arekalov <artyom.rekalov@gmail.com>
2024-02-25 19:57:57 +0300

    add navigation graph

---
+++ app/src/main/java/com/pro/film_viewer/activities/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.pro.film_viewer.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.pro.film_viewer.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        val navController = Navigation.findNavController(this, R.id.nav_host)
+        NavigationUI.setupWithNavController(bottomNavigation, navController)
+    }
+}
